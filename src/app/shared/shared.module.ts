9722a7e5d348ad49aec1992987217867ce984a57
Hepsox <marie.delaire@outlook.com>
2025-04-15 00:05:01 +0200

    Ajout du composant Header avec styles, template et tests, mise à jour du module partagé et ajout de la couleur de fond dans les styles globaux

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [FooterComponent],
-  exports: [FooterComponent],
+  declarations: [FooterComponent, HeaderComponent],
+  exports: [FooterComponent, HeaderComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
