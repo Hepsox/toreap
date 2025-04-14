9b271e7b5ec08f8cfa31b667677a2ec17f3e5524
Hepsox <marie.delaire@outlook.com>
2025-04-14 23:35:21 +0200

    Ajout du composant Home avec styles et template, mise à jour du module partagé et remplacement du formulaire de connexion par le composant Home dans le composant principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ButtonComponent } from './button/button.component';
 import { CardHorizontalComponent } from './card-horizontal/card-horizontal.component';
 import { AuthModule } from './auth/auth.module';
 import { LoginFormComponent } from './auth/components/login-form/login-form.component';
+import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,9 @@ import { LoginFormComponent } from './auth/components/login-form/login-form.comp
     CardComponent,
     ButtonComponent,
     CardHorizontalComponent,
+    HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
